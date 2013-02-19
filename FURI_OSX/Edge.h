e5ac28627f65866b92bf50724e0de0d39812c02c
Spencer Prost <spencer.prost@live.com>
2013-02-19 00:31:08 -0700

    Extensive Revisions

--- FURI_OSX/Edge.h
+++ FURI_OSX/Edge.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <vector>
-#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/highgui/highgui.hpp"
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/opencv.hpp>
+#include "opencv\cv.h"
 using namespace std;
 
 class Edge
--- FURI_OSX/Utility.h
+++ FURI_OSX/Utility.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <windows.h>
+#include <time.h>
 #include <math.h>
 
 class Utility
